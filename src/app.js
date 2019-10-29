d156e3f2c0377f8cbfb382cb133eac0067ca5289
ddingchen <183488175@qq.com>
2019-10-29 13:25:44 +0800

    try

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-// export class Scene {
-//     constructor() {
-//         console.error('web c3d')
-//     }
-// }
-
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+export class Scene {
+    constructor() {
+        console.error('web c3d')
+    }
 }
+
+// exports.printMsg = function() {
+//   console.log("This is a message from the demo package");
+// }
